605f7ee53e4649029f1e68fbce0a9d164391576a
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-03-05 19:05:27 +0300

    Some changes

--- ADOConsole/Program.cs
+++ ADOConsole/Program.cs
@@ -5,6 +5,14 @@
         static void SelectData()
         {
             // Benim bu ortamdan SQL Server ortamına bağlanabilmem için
+            // en gerekli seyler
+            // 1. Sql e bağlanabilmek için bir ConnectionString tanımı
+            // 2. Bir SqlConnection nesnesi
+
+            string vs_ConnStr = "";
+
+            // unutulma durumunda www.connectionstrings.com
+            vs_ConnStr = "Server=myServerAddress;Database=Northwind;Trusted_Connection=True;";
 
 
 
