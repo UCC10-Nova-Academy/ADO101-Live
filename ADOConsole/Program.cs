fb48ef2156be46e72f64a8d1b2a30427f0ea2c2c
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-03-05 18:54:35 +0300

    Add project files.

---
+++ ADOConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ADOConsole
+{
+    internal class Program
+    {
+        static void SelectData()
+        {
+            // Benim bu ortamdan SQL Server ortamına bağlanabilmem için
+
+
+
+
+
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
